da933cc1bc22df8424a3f4abbd03ac4424a4f1c3
ManasaR08 <manasar259@gmail.com>
2021-10-27 12:49:00 +0530

    dynamic view logic

--- myproject/challenges/views.py
+++ myproject/challenges/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+monthly_challenges = {
+    "january": "Welcome to the January challenge",
+    "february": "Welcome to the February challenge",
+    "march": "Welcome to the March challenge",
+    "april": "Welcome to the April challenge",
+    "may": "Welcome to the May challenge",
+    "june": "Welcome to the June challenge",
+    "july": "Welcome to the July challenge",
+    "august": "Welcome to the August challenge",
+    "september": "Welcome to the September challenge",
+    "october": "Welcome to the October challenge",
+    "november": "Welcome to the November challenge",
+    "december": "Welcome to the December challenge"
+}
 # Create your views here.
 
 def monthly_challenge_by_number(request, month):
@@ -8,13 +22,9 @@ def monthly_challenge_by_number(request, month):
 
 
 def monthly_challenge(request, month):
-    if month == 'january':
-        return HttpResponse("Welcomw to January")
-    elif month == 'february':
-        return HttpResponse("Welcomw to February")
-    elif month == 'march':
-        return HttpResponse("Welcomw to March")
-    else:
-        return HttpResponseNotFound("This month is not supported")
+    try:
+        challenge_text = monthly_challenges[month]
+    except:
+        return HttpResponseNotFound("Plase enter a valid month")
     return HttpResponse(challenge_text)
 
